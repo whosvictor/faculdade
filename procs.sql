5f1c22a22a27c197b81e9c124a7a9d78f6f8ea1f
Victor Freires <77703478+whosvictor@users.noreply.github.com>
2022-09-22 20:10:46 -0300

    Create procs.sql

---
+++ procs.sql
@@ -0,0 +1,26 @@
+USE `classicmodels`;
+
+DELIMITER $$
+
+CREATE PROCEDURE Selecionar_Produtos(in quantidade INT)
+begin 
+	select * from products limit quantidade;
+end$$
+DELIMITER ; 
+
+CALL selecionar_produtos(10);
+
+
+
+
+DELIMITER $$
+
+CREATE PROCEDURE contagem_Produtos(OUT quantidade INT)
+begin 
+	select count(*) from products limit quantidade;
+end$$
+DELIMITER ; 
+
+CALL contagem_produtos(@total);
+select @total;
+
